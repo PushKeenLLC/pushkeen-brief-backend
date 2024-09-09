aa6a5bb33336af48c31f8b4706e3eb9f3fe9e3a9
pitersizovdev <pitersizov@gmail.com>
2024-09-09 15:26:39 +0300

    date_logging

--- controllers/briefController.js
+++ controllers/briefController.js
@@ -93,8 +93,20 @@ exports.sendBrief = async (req, res) => {
 
     await sendEmail(attachments, subject, message, send_to, sent_from, reply_to);
 
+    // Логирование даты и времени отправки, отправителя и получателя
+    const now = new Date().toISOString();
+    console.log(
+      `[${now}] Brief sent successfully. Subject: ${subject}, From: ${sent_from}, To: ${send_to.join(
+        ', '
+      )}`
+    );
+
     res.status(200).json({ success: true, message: 'Brief Sent' });
   } catch (error) {
+    const now = new Date().toISOString();
+    console.error(
+      `[${now}] Failed to send brief. Error: ${error.message}, From: ${process.env.EMAIL_HEY}, To: ${process.env.EMAIL_HEY}, ${process.env.EMAIL_KPM}`
+    );
     res.status(500).json({ success: false, message: error.message });
   }
 };
--- controllers/emailController.js
+++ controllers/emailController.js
@@ -26,14 +26,20 @@ exports.sendEmail = async (req, res) => {
 
     await sendEmail(attachments, subject, message, send_to, sent_from, reply_to);
 
-    // Логирование даты и времени отправки
+    // Логирование даты и времени отправки, отправителя и получателя
     const now = new Date().toISOString();
-    console.log(`[${now}] Email sent successfully. Subject: ${subject}`);
+    console.log(
+      `[${now}] Email sent successfully. Subject: ${subject}, From: ${sent_from}, To: ${send_to.join(
+        ', '
+      )}`
+    );
 
     res.status(200).json({ success: true, message: 'Email Sent' });
   } catch (error) {
     const now = new Date().toISOString();
-    console.error(`[${now}] Failed to send email. Error: ${error.message}`);
+    console.error(
+      `[${now}] Failed to send email. Error: ${error.message}, From: ${process.env.EMAIL_HEY}, To: ${process.env.EMAIL_HEY}, ${process.env.EMAIL_KPM}`
+    );
     res.status(500).json({ success: false, message: error.message });
   }
 };
