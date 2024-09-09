bc3e7a3f89898caf5b3de9bbb770d367bcfe7be6
Daniil Sytaev <danya.sytaev@gmail.com>
2024-09-09 15:51:51 +0300

    add: (briefController) - edit message

--- controllers/briefController.js
+++ controllers/briefController.js
@@ -1,4 +1,4 @@
-const sendEmail = require('../utils/sendEmail');
+const sendEmail = require("../utils/sendEmail");
 
 exports.sendBrief = async (req, res) => {
   const {
@@ -29,47 +29,46 @@ exports.sendBrief = async (req, res) => {
     const send_to = [process.env.EMAIL_HEY, process.env.EMAIL_KPM];
     const sent_from = process.env.EMAIL_HEY;
     const reply_to = process.env.EMAIL_HEY;
-    const subject = 'Бриф с сайта pushkeen.ru';
+    const subject = "Бриф с сайта pushkeen.ru";
 
     const message = `
-      <p><b>Имя</b>: ${name || '-'}</p>
-      <p><b>Телефон</b>: ${phone || '-'}</p>
-      <p><b>Почта</b>: ${email || '-'}</p>
-      <p><b>Название проекта</b>: ${projectName || '-'}</p>
+      <p><b>Имя</b>: ${name || "-"}</p>
+      <p><b>Телефон</b>: ${phone || "-"}</p>
+      <p><b>Почта</b>: ${email || "-"}</p>
+      <p><b>Название проекта</b>: ${projectName || "-"}</p>
 
       </br></br>
       <p>КОМПАНИЯ И РЫНОК</p>
-      <p><b>Название вашей компании</b>: ${companyName || '-'}</p>
-      <p><b>Расскажите, чем занимается ваша компания?</b>: ${fieldOfActivity || '-'}</p>
-      <p><b>Расскажите, какие продукты/услуги вы предлагаете клиентам?</b>: ${products || '-'}</p>
-      <p><b>Кто ваши клиенты? Расскажите о сегментах вашей аудитории:</b> ${clients || '-'}</p>
-      <p><b>Кто ваши ключевые конкуренты?</b>: ${competitors || '-'}</p>
-      <p><b>Какое преимущество выделяет вас среди конкурентов</b>: ${advantages || '-'}</p>
+      <p><b>Название вашей компании</b>: ${companyName || "-"}</p>
+      <p><b>Расскажите, чем занимается ваша компания?</b>: ${fieldOfActivity || "-"}</p>
+      <p><b>Расскажите, какие продукты/услуги вы предлагаете клиентам?</b>: ${products || "-"}</p>
+      <p><b>Кто ваши клиенты? Расскажите о сегментах вашей аудитории:</b> ${clients || "-"}</p>
+      <p><b>Кто ваши ключевые конкуренты?</b>: ${competitors || "-"}</p>
+      <p><b>Какое преимущество выделяет вас среди конкурентов</b>: ${advantages || "-"}</p>
 
       </br></br>
       <p>ОЖИДАНИЯ</p>
-      <p><b>Какую задачу должен решать продукт/проект?</b>: ${tasks || '-'}</p>
-      <p><b>В какие сроки нужно завершить работу?</b>: ${deadlines || '-'}</p>
-      <p><b>Рассматриваете ли вы предложения от других компаний?</b>: ${offers || '-'}</p>
+      <p><b>Какую задачу должен решать продукт/проект?</b>: ${tasks || "-"}</p>
+      <p><b>В какие сроки нужно завершить работу?</b>: ${deadlines || "-"}</p>
+      <p><b>Рассматриваете ли вы предложения от других компаний?</b>: ${offers || "-"}</p>
 
       ${
-        briefType?.includes('Web-разработка')
+        briefType?.includes("Web-разработка")
           ? `
           </br></br>
           <p>УСЛУГА: ВЕБ-РАЗРАБОТКА</p>
           <p><b>Техническое задание (ТЗ):</b> ${
-            req.files?.technicalSpecifications
-              ? `Название файла - ${req.files.technicalSpecifications[0].originalname}`
-              : '-'
+            req.files?.technicalSpecifications ? `Название файла - ${req.files.technicalSpecifications[0].originalname}` : "-"
           }</p>
-          <p><b>Предполагаемые разделы сайта:</b> ${sections || '-'}</p>
-          <p><b>Материалы для сайта:</b> ${stuff || '-'}</p>
-          <p><b>Любимые сайты:</b> ${favSites || '-'}</p>
-          <p><b>Нелюбимые сайты:</b> ${hateSites || '-'}</p>
-          <p><b>Необходимые функции:</b> ${features ? features.join(', ') : '-'}</p>
-          <p><b>Другие пожелания:</b> ${other || '-'}</p>
+          <p><b>Предполагаете ли вы создание одностраничного или многостраничного сайта?</b> ${site || "-"}</p>
+          <p><b>Предполагаемые разделы сайта:</b> ${sections || "-"}</p>
+          <p><b>Материалы для сайта:</b> ${stuff || "-"}</p>
+          <p><b>Любимые сайты:</b> ${favSites || "-"}</p>
+          <p><b>Нелюбимые сайты:</b> ${hateSites || "-"}</p>
+          <p><b>Необходимые функции:</b> ${features ? features.join(", ") : "-"}</p>
+          <p><b>Другие пожелания:</b> ${other || "-"}</p>
         `
-          : ''
+          : ""
       }
     `;
 
@@ -95,13 +94,9 @@ exports.sendBrief = async (req, res) => {
 
     // Логирование даты и времени отправки, отправителя и получателя
     const now = new Date().toISOString();
-    console.log(
-      `[${now}] Brief sent successfully. Subject: ${subject}, From: ${sent_from}, To: ${send_to.join(
-        ', '
-      )}`
-    );
+    console.log(`[${now}] Brief sent successfully. Subject: ${subject}, From: ${sent_from}, To: ${send_to.join(", ")}`);
 
-    res.status(200).json({ success: true, message: 'Brief Sent' });
+    res.status(200).json({ success: true, message: "Brief Sent" });
   } catch (error) {
     const now = new Date().toISOString();
     console.error(
