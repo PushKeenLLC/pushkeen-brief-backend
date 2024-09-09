4b42a4d6c071e2dba7ce4c404a85ac0ee2d3f0b3
pitersizovdev <pitersizov@gmail.com>
2024-09-09 11:32:31 +0300

    rearch

---
+++ controllers/emailController.js
@@ -0,0 +1,31 @@
+const sendEmail = require("../utils/sendEmail");
+
+exports.sendEmail = async (req, res) => {
+  const { name, phone, email, text, services } = req.body;
+  try {
+    const send_to = [process.env.EMAIL_HEY, process.env.EMAIL_KPM];
+    const sent_from = process.env.EMAIL_HEY;
+    const reply_to = process.env.EMAIL_HEY;
+    const subject = "Бриф  с сайта pushkeen.ru";
+
+    const message = `
+      <p><b>Имя</b>: ${name}</p>
+      <p><b>Телефон</b>: ${phone}</p>
+      <p><b>Почта</b>: ${email}</p>
+      <p><b>Описание идеи</b>: ${text}</p>
+      <p><b>Тип услуги</b>: ${services}</p>
+    `;
+    const attachments = [
+      {
+        filename: req.files?.file.name,
+        content: req.files?.file.data,
+        contentType: req.files?.file.mimetype,
+      },
+    ];
+
+    await sendEmail(attachments, subject, message, send_to, sent_from, reply_to);
+    res.status(200).json({ success: true, message: "Email Sent" });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
---
+++ routes/api.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const emailController = require("../controllers/emailController");
+const briefController = require("../controllers/briefController");
+
+router.get("/", (_, res) => {
+  res.send("Home Page");
+});
+
+router.post("/sendemail", emailController.sendEmail);
+router.post("/brief", briefController.sendBrief);
+
+module.exports = router;
